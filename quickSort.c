e150714808586afdabef25a6b5b9b18e4ef2ba1f
Nicolas Duflos <nicolas.duflos@orange.fr>
2015-12-02 16:43:31 +0100

    Fin de TP productif!

---
+++ quickSort.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "pile.h"
+#include "tri.h"
+
+#define MAX_BLOCS 100
+
+
+void rapide(int tab[MAX_BLOCS]){
+	pthread_t[MAX_BLOCS] ths;
+	bloc_t bloc;
+	pile pile;
+
+	//creer une pile
+	init_pile(pile);
+	
+
+	//creer n threads
+	for (i = 0; i < n; i++){
+		ths[i] = pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *)));
+	}
+
+	//Verifier si la pile n'est pas vide
+	if (!pile_vide(pile)){
+		
+	}
+}
+
+
